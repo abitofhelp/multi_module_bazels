feb2ab407b5391b789ae32550666917328c76874
Mike Gardner <mjgardner@abitofhelp.com>
2023-11-21 21:38:44 -0700

    Added cmd project

---
+++ cmd/cmd.go
@@ -0,0 +1,25 @@
+package cmd
+
+func main() {
+
+}
+
+//func Howdy() string {
+//	return "Howdy!"
+//}
+//
+//func HumanizedLargeValue() string {
+//	return humanize.Comma(int64(12345678))
+//}
+//
+//func GetUtcNowAsRfc3339() string {
+//	return time.Now().UTC().Format(time.RFC3339)
+//}
+//
+//func GetPhoenixNowAsRfc3339() (string, error) {
+//	if loc, err := time.LoadLocation("America/Phoenix"); err == nil {
+//		return time.Now().In(loc).Format(time.RFC3339), nil
+//	} else {
+//		return "", errors.New("failed to determine the current time in Phoenix, Arizona, USA")
+//	}
+//}
---
+++ cmd/go.mod
@@ -0,0 +1,5 @@
+module github.com/abitofhelp/multimod/cmd
+
+go 1.21
+
+require github.com/dustin/go-humanize v1.0.1
